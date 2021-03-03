12bbaba39571642709d8ebc61ce518e1e2d75349
HugoLd <ledall.hugo.pro@gmail.com>
2021-03-03 15:38:25 +0100

    Init app

---
+++ src/main/java/org/hld/hugold/HugoLdApplication.java
@@ -0,0 +1,13 @@
+package org.hld.hugold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HugoLdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HugoLdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hld/hugold/HugoLdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hld.hugold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HugoLdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
