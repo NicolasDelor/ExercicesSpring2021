12deeccfa8274acda3d9c424ed3a18f139a8e5e7
Nicolas <nicolas@hotmail.fr>
2021-04-26 11:55:08 +0200

    add project

---
+++ nicolasd/src/main/java/org/hld/nicolasd/NicolasdApplication.java
@@ -0,0 +1,13 @@
+package org.hld.nicolasd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicolasdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicolasdApplication.class, args);
+	}
+
+}
